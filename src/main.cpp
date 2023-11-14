05399bcfb3a5c10911ee875f5c6013a4676184d0
gaelboisson <gael.boisson.77@gmail.com>
2023-11-14 15:12:18 +0100

    Récupération taille écran et affichage fenêtre SDL

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,36 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 
-
 int main(int argc, char* argv[]) {
-    
+    // Initialisation de SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("Erreur lors de l'initialisation de SDL : %s", SDL_GetError());
+        return -1;
+    }
+
+    // Création de la fenêtre SDL
+    SDL_Window* fenetre = SDL_CreateWindow("Ma fenêtre SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    if (!fenetre) {
+        SDL_Log("Erreur lors de la création de la fenêtre : %s", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    // Boucle principale
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        // Dessiner ici...
+    }
+
+    // Libération des ressources et fermeture de SDL
+    SDL_DestroyWindow(fenetre);
+    SDL_Quit();
     return 0;
 }
